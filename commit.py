1732dc43e0d762e3b781b4560d5824d4c9930aa2
Kafonek <kafonek@gmail.com>
2016-07-27 19:30:53 -0400

    updating

---
+++ commit.py
@@ -0,0 +1,18 @@
+import glob
+files = ['main.py'] + [f for f in glob.glob('*.py') if f not in ['main.py', 'commit.py']]
+
+
+from subprocess import Popen, PIPE
+def run(cmd):
+	proc = Popen(cmd, stdout=PIPE, shell=True)
+	text = proc.communicate()[0]
+	return text
+
+	
+cmd = 'rapydscript %s -pbm' % ' '.join(files)
+run(cmd)
+
+run('git add .')
+run('git commit -m "updating" .')
+run('git push')
+
