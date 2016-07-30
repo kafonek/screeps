88565cd0d5d29cdb5b45116587aab74eb7700867
Kafonek <kafonek@gmail.com>
2016-07-29 20:39:41 -0400

    updates

--- sim/js/main.js
+++ sim/js/main.js
@@ -2,6 +2,5 @@
 var __name__ = "__main__";
 
 function main() {
-    console.log("updated again");
 }
 module.exports.loop = main();
--- sim/main.py
+++ sim/main.py
@@ -1,5 +1,5 @@
 def main():
-	console.log('updated again')
+	pass
 
 	
 
