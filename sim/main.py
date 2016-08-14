d26669064dce61529f62c6e81cf57c80dd69846e
Kafonek <kafonek@gmail.com>
2016-08-14 12:05:26 -0400

    updates

--- sim/main.py
+++ sim/main.py
@@ -1,6 +1,6 @@
 def main():
 	if Game.time % 5 == 0:
-		console.log("Game tick: %s" % Game.time)
+		console.log("Game tick: " + str(Game.time))
 	#console.log('working')
 	#for name in Game.rooms:
 		#console.log(name)
