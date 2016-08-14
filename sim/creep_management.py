16da275e41285d0866a05320360fbff1ab96af55
Kafonek <kafonek@gmail.com>
2016-08-14 13:51:49 -0400

    updates

--- sim/creep_management.py
+++ sim/creep_management.py
@@ -3,7 +3,8 @@ class CreepManager:
 		self.room = Game.rooms[roomname]
 		self.creeps = self.room.find(FIND_MY_CREEPS)
 		self.spawn = self.room.find(FIND_MY_SPAWNS)[0]
-		print(dir(self.spawn))
+		if Game.time % 10 == 0:
+			print(dir(self.spawn))
 
 	def spawnManager(self):
 		pass	
--- sim/js/main.js
+++ sim/js/main.js
@@ -107,7 +107,9 @@ CreepManager.prototype.__init__ = function __init__(roomname){
     self.room = Game.rooms[roomname];
     self.creeps = self.room.find(FIND_MY_CREEPS);
     self.spawn = self.room.find(FIND_MY_SPAWNS)[0];
-    __rapydscript_print(dir(self.spawn));
+    if (Game.time % 10 === 0) {
+        __rapydscript_print(dir(self.spawn));
+    }
 };
 CreepManager.prototype.spawnManager = function spawnManager(){
     var self = this;
