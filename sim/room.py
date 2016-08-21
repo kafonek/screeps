b639cb046c8cdb7d4a1694127f9f39ad3b2a2d5c
Kafonek <kafonek@gmail.com>
2016-08-21 17:47:29 -0400

    updates

--- sim/room.py
+++ sim/room.py
@@ -1,11 +1,11 @@
-def memcache(fn):
-	print("In memcache of " + fn)
-	def wrapped(self, *args, **kwargs):
-		return fn(self, *args, **kwargs)
+#def memcache(fn):
+#	print("In memcache of " + fn)
+#	def wrapped(self, *args, **kwargs):
+#		return fn(self, *args, **kwargs)
 
 class Room:
 	
-	@memcache
+#	@memcache
 	def stats(self):
 		return {'creep_count' : len(self.find(FIND_MY_CREEPS))}
 			
